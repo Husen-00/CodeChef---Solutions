059a63091ebfa2ddd84b9303e7ad83151bf72388
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-03-22 15:14:08 +0530

    Add Armstrong number checker in Check_Armstrong_or_not.c

---
+++ Check_Armstrong_or_not.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+    int n, r, sum = 0, temp;
+
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    temp = n;
+
+    while(n > 0)
+    {
+        r = n % 10;
+        sum = sum + (r*r*r);
+        n = n / 10;
+    }
+
+    if(sum == temp)
+        printf("Armstrong number");
+    else
+        printf("Not Armstrong");
+
+    return 0;
+}
